f71a40b788f34cc8b86172a9955167d65a5b6b75
Mostafa Hamed <mostafa.hamed@esmaar.com>
2019-01-06 14:31:34 +0200

    e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -106,3 +106,28 @@ function getActual(){
     count: logs.count()
   };
 }
+
+it('should not emit the event initially', function(){
+  let voteLabel = element(by.tagName('app-vote-taker'))
+       .element(by.tagName('h3')).getText();
+  expect(voteLabel).toBe('Agree: 0, Disagree: 0');
+});
+
+it('should process Agree vote', function(){
+  let agreeButton1 = element.all(by.tagName('app-voter')).get(0)
+      .all(by.tagName('button')).get(0);
+  agreeButton1.click().then(function(){
+    let voteLabel = element(by.tagName('h3')).getText();
+  expect(voteLabel).toBe('Agree: 1, Disagree: 0');
+  });
+});
+
+it('should process Disagree vote', function(){
+  let agreeButton1 = element.all(by.tagName('app-voter')).get(1)
+     .all(by.tagName('button')).get(1);
+  agreeButton1.click().then(function(){
+    let voteLabel = element(by.tagName('app-vote-taker'))
+       .element(by.tagName('h3')).getText();
+    expect(voteLabel).toBe('Agree: 1, Disagree: 1');
+  });
+});

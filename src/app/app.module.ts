011ec630c01c8ed6425eb4f336cb7cf497466abf
mostafa <mostafahamed73@yahoo.com>
2019-01-07 07:54:43 +0200

    Parent interacts with child via local variable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import {HeroChildComponent} from './hero-child.component';
-import {HeroParentComponent} from './hero-parent.component';
-import {NameChildComponent} from './name-child.component';
-import {NameParentComponent} from './name-parent.component';
-import {VersionChildComponent} from './version-child.component';
-import {VersionParentComponent} from './version-parent.component';
-import {VoterComponent} from './voter.component';
-import {VoteTakerComponent} from './votetaker.component';
+import { AppComponent }                   from './app.component';
+import {HeroChildComponent}               from './hero-child.component';
+import {HeroParentComponent}              from './hero-parent.component';
+import {NameChildComponent}               from './name-child.component';
+import {NameParentComponent}              from './name-parent.component';
+import {VersionChildComponent}            from './version-child.component';
+import {VersionParentComponent}           from './version-parent.component';
+import {VoterComponent}                   from './voter.component';
+import {VoteTakerComponent}               from './votetaker.component';
+import {CountdownTimerComponent}          from './countdown-timer.component';
+import {CountdownLocalVarParentComponent} from './countdown-parent.component';
 
 let directives: any[] = [
   AppComponent,
@@ -20,7 +22,9 @@ let directives: any[] = [
   VersionChildComponent,
   VersionParentComponent,
   VoterComponent,
-  VoteTakerComponent
+  VoteTakerComponent,
+  CountdownTimerComponent,
+  CountdownLocalVarParentComponent
 ]
 @NgModule({
   declarations: directives,
---
+++ src/app/countdown-parent.component.ts
@@ -0,0 +1,14 @@
+import { Component }               from '@angular/core';
+import { CountdownTimerComponent } from './countdown-timer.component';
+
+@Component({
+    selector: 'app-countdown-parent-lv',
+    template: `
+    <h3>Countdown to liftoff (via local variable)</h3>
+    <button (click) = "timer.start()">Start</button>
+    <button (click) = "timer.stop()">Stop</button>
+    <div> {{timer.seconds}}</div>
+    <app-countdown-timer #timer></app-countdown-timer>
+    `
+})
+export class CountdownLocalVarParentComponent {}
